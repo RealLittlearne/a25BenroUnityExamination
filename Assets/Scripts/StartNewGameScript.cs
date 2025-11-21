5a2914c5af981832babb43f2086e371dc93aa5b9
RealLittlearne <info@littlearne.com>
2025-11-21 14:53:10 +0100

    Wrote code to the scirpt

--- Assets/Scripts/StartNewGameScript.cs
+++ Assets/Scripts/StartNewGameScript.cs
@@ -1,16 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class StartNewGameScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public UnityEvent OnStartNewGame;
+    private float timerBeforeSceneChange = 2f;
+    private bool timerHasStarted;
+
+    public void Update()
     {
-        
+        if (timerHasStarted)
+        {
+            timerBeforeSceneChange -= Time.deltaTime;
+            if (timerBeforeSceneChange <= 0)
+            {
+                SceneManager.LoadSceneAsync("Level1", LoadSceneMode.Single);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeScene()
     {
-        
+        OnStartNewGame?.Invoke();
+        timerHasStarted = true;
     }
 }
